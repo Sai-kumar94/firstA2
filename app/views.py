bec1f2c78135f69920c8fbd1ef0d33ee07294d67
Sai-kumar94 <92421562+Sai-kumar94@users.noreply.github.com>
2024-11-22 15:16:34 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def sai(request):
+    return HttpResponse('this is sai')
+def durga(request):
+    return HttpResponse('<marquee>Prostitute Kopma Owner</marquee>')
