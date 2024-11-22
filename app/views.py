f9af17476113b86d86908a037ef31d7c664951bb
Sai-kumar94 <92421562+Sai-kumar94@users.noreply.github.com>
2024-11-22 15:49:24 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 def sai(request):
     return HttpResponse('this is sai')
 def durga(request):
-    return HttpResponse('<marquee>Prostitute Kopma Owner</marquee>')
+    return HttpResponse('<marquee>MG</marquee>')
